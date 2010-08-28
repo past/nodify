13a0295ba39fcf9fa2c535649e2948f42e7ca0dc
Panagiotis Astithas <pastith@gmail.com>
2010-08-28 11:36:14 +0300

    Use port 8080 as God Intended.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http'),
-	port = parseInt(process.env.PORT) || 8001;
+	port = parseInt(process.env.PORT) || 8080;
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
