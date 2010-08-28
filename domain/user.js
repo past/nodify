312d77590e1b9df1c6dc85f695766bbf94a41802
Christos Stathis <chstath@gmail.com>
2010-08-28 13:05:35 +0300

    Fixed some typos

--- domain/user.js
+++ domain/user.js
@@ -5,22 +5,22 @@ var User = exports.User = function(username) {
     this.username = username;
     this.projects = {};
     this.projects.length = 0;
-}
+};
 
 User.prototype.addProject = function(project) {
     this.projects[project.id] = project;
     this.projects.length++;
     this.projects.lastProject = project;
-}
+};
 
 User.prototype.removeProject = function(project) {
     delete this.projects[project.id];
     this.projects.length--;
-}
+};
 
 User.prototype.toString = function() {
     return this.username;
-}
+};
 
 Object.defineProperty(User.prototype, "id", {
     get: function() {
