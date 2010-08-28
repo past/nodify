471707e850a707ec56f0b0c84f012b2bbd027f30
Panagiotis Astithas <pastith@gmail.com>
2010-08-28 10:42:44 +0300

    Add a hello world example to kickstart things.

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.end("Hello world\n");
+});
+
+server.listen(8080);
+
